5fb012766891a310d0540f8feddd3bb68c5f90e0
Natalie Conde <black_star_big@hotmail.com>
2017-02-12 13:20:56 -0600

    js/ - se agrego pantalla signup

--- js/mover.js
+++ js/mover.js
@@ -1,8 +1,32 @@
-function Mover(){
+function MoverLI(){
 	$(".login").addClass('animacionLI');
 	$(".yeslogin").addClass('animacionYLI');
 	}
 
 $(document).ready(function(){
-    $(".botonLI").click(Mover);
+    $(".botonLI").click(MoverLI);
+});
+
+
+function MoverSU(){
+	$(".login").addClass('animacionLI');
+	$(".registro").addClass('animacionSU');
+	}
+
+$(document).ready(function(){
+    $(".botonSU").click(MoverSU);
+});
+
+
+function MoverSUtoLI(){
+	$(".registro").addClass('animacionSUtoLI');
+	$(".yeslogin").addClass('animacionYLI');
+
+	$(".yeslogin").css("z-index", "102");
+	$(".registro").addClass('hideLogin');
+	$(".login").addClass('hideLogin');
+	}
+
+$(document).ready(function(){
+    $(".loginback").click(MoverSUtoLI);
 });
