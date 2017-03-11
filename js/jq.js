6d51b9fc21b0b7ef24f9263cdab042be0eeca455
RHR95 <rochernan@hotmai.com>
2017-03-10 19:48:12 -0600

    cosita para arriba

---
+++ js/jq.js
@@ -0,0 +1,50 @@
+
+
+
+
+$(document).ready(function(){
+
+
+
+ var panel1 = $(" .landPanel-1");
+  var panel2 = $(" .landPanel-2");
+  var panel3 = $(" .landPanel-3");
+  var panel4 = $(" .landPanel-4");
+
+
+
+
+  $(".Landpage").hide();
+   panel1.show();
+
+
+
+
+
+$(".btn").click(function() {
+	/*this.stop().animate({scrollTop:0}, '500', 'swing', function() { 
+						});*/
+
+						  $(".landPanel-2").show();
+
+					panel1.slideUp( "slow", function() {
+						 
+						    
+						  });
+					
+
+						
+
+						
+						
+   
+});
+
+
+
+
+
+
+
+
+});
